a917c6f98f0783c87fd3e22abd68a198bdc7cbdb
SnuggleTrouble <90455892+SnuggleTrouble@users.noreply.github.com>
2023-08-17 16:02:26 +0200

    implement Table & fix function name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./compontents/Header/Header";
 import UserInput from "./compontents/UserInput/UserInput";
+import Table from "./compontents/Table/Table";
 
 function App() {
   const calculateHandler = (userInput) => {
@@ -39,26 +40,7 @@ function App() {
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table />
     </div>
   );
 }
