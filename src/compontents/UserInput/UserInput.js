262cada3343fe097356d71521ecb5004acae9381
SnuggleTrouble <90455892+SnuggleTrouble@users.noreply.github.com>
2023-08-17 16:13:36 +0200

    prevent webpage reload on submit

--- src/compontents/UserInput/UserInput.js
+++ src/compontents/UserInput/UserInput.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const UserInput = () => {
-  const submitHandler = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
     console.log("Submitted successfully")
   };
 
